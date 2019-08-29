91e9ec3201ac346faf9be0b9f5c6db6a9bbe86e1
Joshua <jbkunst@gmail.com>
2019-08-29 17:16:38 -0400

    initial commit :bug:

---
+++ R/00-install-deps.R
@@ -0,0 +1,12 @@
+install.packages(c(
+  # tidy
+  "tidyverse", "tidytext", "broom",
+  # modelling
+  "forecast", "caret", "randomForest", "ranger", "Cubist", "partykit", "h2o", "reticulate",
+  # reporting
+  "shiny", "shinydashboard", "flexdashboard", "rmarkdown", "xaringan",
+  # widgets
+  "highcharter", "DT", "plotly", "dygraphs", "leaflet",
+  # ggplot
+  "ggplot2", "hrbrthemes", "ggmap", "ggrepel", "patchwork"
+), repos = "http://cran.rstudio.com/")
