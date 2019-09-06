e5c6de02b17ab04fbb8763a335c7400644967a57
jbkunst <jbkunst@gmail.com>
2019-09-05 21:30:11 -0300

    lala

--- R/clase-02-mi-primer-script.R
+++ R/clase-02-mi-primer-script.R
@@ -1,1 +1,25 @@
 # aca van las instrucciones
+
+# por ej asignacion 
+# la combinaciòn magina CTRL+ENTER
+x <- 4
+
+# paquetes:
+# 
+plot(cumsum(rnorm(100)), type = "l")
+
+# primer paquete a instalar el "tidyverse":
+# es una coleccion de paquetes
+# install.packages("tidyverse")
+library(tidyverse)
+
+
+url <- "https://docs.google.com/spreadsheets/d/1amKJa4Ua4qO87z3OL-OEv3EznanqOoTy0L-NUimoowM/export?format=tsv&id=1amKJa4Ua4qO87z3OL-OEv3EznanqOoTy0L-NUimoowM&gid=2134224185"
+
+url
+
+nchar(url)
+
+data <- read_tsv(url)
+
+glimpse(data)
