0be6af6f48274ed845ae41c749476cd6d6012249
jbkunst <jbkunst@gmail.com>
2019-10-09 18:49:42 -0300

    subiendo datos

--- R/clase-02-mi-primer-script.R
+++ R/clase-02-mi-primer-script.R
@@ -13,13 +13,62 @@ plot(cumsum(rnorm(100)), type = "l")
 # install.packages("tidyverse")
 library(tidyverse)
 
-
 url <- "https://docs.google.com/spreadsheets/d/1amKJa4Ua4qO87z3OL-OEv3EznanqOoTy0L-NUimoowM/export?format=tsv&id=1amKJa4Ua4qO87z3OL-OEv3EznanqOoTy0L-NUimoowM&gid=2134224185"
 
 url
 
-nchar(url)
+# nchar(url)
 
 data <- read_tsv(url)
 
 glimpse(data)
+
+
+# cambiar nombres ---------------------------------------------------------
+data <- data %>% 
+  rename_all(str_replace_all, " ", "_") %>% 
+  rename_all(str_remove_all, "\\?") %>% 
+  rename_all(str_remove_all, "!") %>% 
+  rename_all(stringi::stri_trans_general, id = "Latin-ASCII") %>% 
+  rename_all(str_to_lower)
+
+glimpse(data)
+    
+# x <- "asdasdf arasdfa ert@zsv.cl weadfasd wer@sdf.quetimpornta"
+# 
+# str_extract_all(x, "[a-z]+@[a-z]+\\.[a-z]+")
+
+
+# que edad tenemos --------------------------------------------------------
+# - necesitamos que la variable sea interpretada como fecha
+library(lubridate)
+
+class(mdy("2/22/1995"))
+
+data <- data %>% 
+  mutate(fecha_de_nacimiento = mdy(fecha_de_nacimiento))
+
+glimpse(data)
+
+
+as.numeric(difftime(Sys.Date(), mdy("2/22/1995"), unit="weeks"))/52.25
+
+
+data <- data %>% 
+  mutate(edad = as.numeric(difftime(Sys.Date(), fecha_de_nacimiento, unit="weeks"))/52.25
+)
+
+glimpse(data)
+
+data %>% 
+  summarise(mean(edad, na.rm = TRUE))
+
+dg1 <- data %>% 
+  filter(ramo_que_mas_amo_en_la_vida == "Procesos estocásticos, estadística y probabilidad")
+
+
+
+ggplot(data) +
+  geom_point(aes(x = edad, ano_de_carrera, 
+                 color = ramo_que_mas_amo_en_la_vida)) +
+  facet_wrap(vars(ramo_que_mas_amo_en_la_vida))
